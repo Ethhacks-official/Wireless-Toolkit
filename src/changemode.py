191239b09123ca8208281aa6aaa322f491130771
Ethhacks-official <Ethhacks.official@gmail.com>
2024-12-20 08:16:24 -0500

    Updated mode changing functionality..

--- src/changemode.py
+++ src/changemode.py
@@ -41,9 +41,23 @@ class ChangeMode:
         mode = Sources().checkmode(wireless_interface)
         if mode == "Managed":
             print(f"\n{BLUE}[+][+]Your wireless adapter mode is successfully changed to 'Managed'.{RESET}")
+            return wireless_interface
         else:
-            print(f"\n{RED}[-][-] Due to some issues, Your wireless adapter mode could not changed to 'Managed'. Trying Again !!.{RESET}")
-            self.changetomanagedmode(wireless_interface)
+            print(f"\n{RED}[-][-] Due to some issues, Your wireless adapter mode could not changed to 'Managed' using aircrack-ng tool. Trying different way !!.{RESET}")
+            print(f"\n{BLUE}[-][-]Changing mode of {wireless_interface} to Managed mode...{RESET}")
+            result = subprocess.run(f"ifconfig {wireless_interface} down", shell=True, capture_output=True, text=True)
+            result = subprocess.run(f"iwconfig {wireless_interface} mode managed", shell=True, capture_output=True, text=True)
+            result = subprocess.run(f"ifconfig {wireless_interface} up", shell=True, capture_output=True, text=True)
+            for interface in list_of_interfaces:
+                if (wireless_interface2_mac_address == Sources().get_dev_path(interface) or ifindex == Sources().get_ifindex(interface)) and wireless_interface2_manufacturer == Sources().get_interface_menufacturer_name(interface) and Sources().checkmode(interface) == "Managed":
+                    wireless_interface = interface
+            mode = Sources().checkmode(wireless_interface)
+            if mode == "Managed":
+                print(f"\n{BLUE}[+][+]Your wireless adapter mode is successfully changed to 'Managed'.{RESET}")
+                return wireless_interface
+            else:
+                print(f"\n{RED}[-][-] Due to some issues, Your wireless adapter mode could not changed to 'Managed'. Trying Again !!.{RESET}")
+                self.changetomanagedmode(wireless_interface)
 
     def changetomonitormode(self, wireless_interface):
         wireless_interface2_manufacturer = Sources().get_interface_menufacturer_name(wireless_interface)
@@ -62,8 +76,22 @@ class ChangeMode:
         mode = Sources().checkmode(wireless_interface)
         if mode == "Monitor":
             print(f"\n{BLUE}[+][+]Your wireless adapter mode is successfully changed to 'Monitor'.{RESET}")
+            return wireless_interface
         else:
-            print(f"\n{RED}[-][-] Due to some issues, Your wireless adapter mode could not changed to 'Managed'. Trying Again !!.{RESET}")
-            self.changetomonitormode(wireless_interface)
+            print(f"\n{RED}[-][-] Due to some issues, Your wireless adapter mode could not changed to 'Monitor' using aircrack-ng tool. Trying different way !!.{RESET}")
+            print(f"\n{BLUE}[-][-]Changing mode of {wireless_interface} to Managed mode...{RESET}")
+            result = subprocess.run(f"ifconfig {wireless_interface} down", shell=True, capture_output=True, text=True)
+            result = subprocess.run(f"iwconfig {wireless_interface} mode monitor", shell=True, capture_output=True, text=True)
+            result = subprocess.run(f"ifconfig {wireless_interface} up", shell=True, capture_output=True, text=True)
+            for interface in list_of_interfaces:
+                if (wireless_interface2_mac_address == Sources().get_dev_path(interface) or ifindex == Sources().get_ifindex(interface)) and wireless_interface2_manufacturer == Sources().get_interface_menufacturer_name(interface) and Sources().checkmode(interface) == "Monitor":
+                    wireless_interface = interface
+            mode = Sources().checkmode(wireless_interface)
+            if mode == "Monitor":
+                print(f"\n{BLUE}[+][+]Your wireless adapter mode is successfully changed to 'Monitor'.{RESET}")
+                return wireless_interface
+            else:
+                print(f"\n{RED}[-][-] Due to some issues, Your wireless adapter mode could not changed to 'Monitor'. Trying Again !!.{RESET}")
+                self.changetomonitormode(wireless_interface)
 
 
